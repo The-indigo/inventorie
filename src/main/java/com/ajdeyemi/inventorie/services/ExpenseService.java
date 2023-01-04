3388dd5401805900710be9e6f96930eb51bc19f8
The-indigo <adepojuadeyemi11@gmail.com>
2023-01-04 18:37:56 -0500

    get all expenses

--- src/main/java/com/ajdeyemi/inventorie/services/ExpenseService.java
+++ src/main/java/com/ajdeyemi/inventorie/services/ExpenseService.java
@@ -3,6 +3,7 @@ package com.ajdeyemi.inventorie.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.ajdeyemi.inventorie.models.Expenses;
 import com.ajdeyemi.inventorie.repositories.ExpensesRepository;
 
 @Service
@@ -10,4 +11,7 @@ public class ExpenseService {
     @Autowired
     ExpensesRepository expensesRepository;
 
+    public Iterable<Expenses> getAllExpenses() {
+        return expensesRepository.findAll();
+    }
 }
